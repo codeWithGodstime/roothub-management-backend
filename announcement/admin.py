7f6053724b57ba1b601ee307371f71f7d00024cd
codeWithGodstime <specialgodstym@gmail.com>
2025-01-08 16:48:40 +0100

    admin field changes for course and announcement

--- announcement/admin.py
+++ announcement/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 from .models import Announcement
 
 
-admin.site.register([Announcement])
+@admin.register(Announcement)
+class AnnouncementAdmin(admin.ModelAdmin):
+    list_display = ('title', 'receiver_group', 'schedule_date', 'schedule_time', 'created_at')
+    list_filter = ('receiver_group', 'schedule_date')
+    search_fields = ('title', 'message')
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'message', 'receiver_group')
+        }),
+        ('Schedule Details', {
+            'fields': ('schedule_date', 'schedule_time')
+        }),
+        ('Important Dates', {
+            'fields': ('created_at', 'updated_at')
+        }),
+    )
--- course/admin.py
+++ course/admin.py
@@ -2,4 +2,59 @@ from django.contrib import admin
 from .models import Course, CourseSession, StudentCourse
 
 
-admin.site.register([Course, CourseSession, StudentCourse])
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'program__name', 'instructor__user', 'duration', 'level', 'created_at')
+    list_filter = ('program', 'duration', 'level')
+    search_fields = ('name', 'program__name', 'instructor__user__fullname')
+    ordering = ('-created_at',)
+    autocomplete_fields = ('instructor', 'program')
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'program', 'instructor')
+        }),
+        ('Details', {
+            'fields': ('duration', 'level')
+        }),
+        ('Important Dates', {
+            'fields': ('created_at', 'updated_at')
+        }),
+    )
+
+
+@admin.register(CourseSession)
+class CourseSessionAdmin(admin.ModelAdmin):
+    list_display = ('course', 'start_date', 'estimated_end_date', 'end_date', 'is_active')
+    list_filter = ('is_active',)
+    search_fields = ('course__name',)
+    ordering = ('-start_date',)
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        (None, {
+            'fields': ('course', 'is_active')
+        }),
+        ('Dates', {
+            'fields': ('start_date', 'estimated_end_date', 'end_date')
+        }),
+        ('Important Dates', {
+            'fields': ('created_at', 'updated_at')
+        }),
+    )
+
+@admin.register(StudentCourse)
+class StudentCourseAdmin(admin.ModelAdmin):
+    list_display = ('student', 'course', 'created_at')
+    search_fields = ('student__user__fullname', 'course__name')
+    autocomplete_fields = ('student', 'course')
+    ordering = ('-created_at',)
+
+# @admin.register(StudentCourseSession)
+# class StudentCourseSessionAdmin(admin.ModelAdmin):
+#     list_display = ('student_course', 'course_session', 'created_at')
+#     search_fields = (
+#         'student_course__student__user__fullname', 
+#         'student_course__course__name',
+#         'course_session__course__name'
+#     )
+#     autocomplete_fields = ('student_course', 'course_session')
+#     ordering = ('-created_at',)
